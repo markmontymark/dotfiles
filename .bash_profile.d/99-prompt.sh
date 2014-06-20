c64617b7dc527ae26067010f195db8542531eec5
Mark Koopman <markmontymark@yahoo.com>
2014-06-19 21:24:02 -0700

    Fix os x scroll with fix to PS1 escape sequences

--- .bash_profile.d/99-prompt.sh
+++ .bash_profile.d/99-prompt.sh
@@ -1,1 +1,4 @@
-PS1='\e[32m`date +%H:%M`\e[0m \e[37m\u\e[0m \e[36m`ellipsisize \W`\e[0m '
+## fix for broken scrolling in os x
+## from: https://discussions.apple.com/thread/3804269, wrap  \e[\d+m\  within \[ and \]
+## for example, \e[32\m  becomes \[\e[32\m\]
+PS1='\[\e[32m\]`date +%H:%M`\[\e[0m\] \[\e[37m\]\u\[\e[0m\] \[\e[36m\]`ellipsisize \W`\[\e[0m\] '
